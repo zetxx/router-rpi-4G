b5054f62a2d2bc09ca08951897488a70332a2c73
Elin Angelov <zetxx@belogradchik.biz>
2018-05-22 19:15:54 +0300

    Update test.js

--- router/app/test.js
+++ router/app/test.js
@@ -7,12 +7,12 @@ const height = 64;
 const address = 0x3C;
 var oled;
 
-const getPixelCoords = ({gsmNetwork, gsmNetworkStatus, vpnStatus, ping, trafficUp, trafficDown, trafficUssed, graph}) => {
+const getPixelCoords = ({gsmNetwork, gsmNetworkStatus, vpnStatus, ping, trafficUp, trafficDown, trafficUsed, graph}) => {
     var d = draw(width, height, '1_8x8');
     d.addText(`NET:${gsmNetwork} ${gsmNetworkStatus === 'connected' ? '✓' : '✗'} | VPN:${vpnStatus === 'connected' ? '✓' : '✗'}`, 0);
     d.addText(`ping -> google: ${ping}`, 8);
     d.addText(`${String.fromCharCode(24)}${trafficUp} ${String.fromCharCode(25)}${trafficDown}`, 17);
-    d.addText(`traffic: ${trafficUssed}`, 25);
+    d.addText(`traffic: ${trafficUsed}`, 25);
     d.addGraph(graph, 0, 34, 64);
     return d.getPoints();
 };
@@ -40,7 +40,7 @@ const redraw = () => {
             ping: (Math.random() * 100000).toString().slice(0, 4) + 'ms',
             trafficUp: (Math.random() * 100000).toString().slice(0, 4) + 'Mb',
             trafficDown: (Math.random() * 100000).toString().slice(0, 6) + 'Mb',
-            trafficUssed: (Math.random() * 100000).toString().slice(0, 2) + '%',
+            trafficUsed: (Math.random() * 100000).toString().slice(0, 2) + '%',
             graph: [new Array(126).fill(0).map((v, idx) => idx), new Array(126).fill(0).map((v, idx) => 100 - idx)]
         }), true))
         .then(isReady)
