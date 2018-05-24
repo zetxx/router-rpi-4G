676f59b6a6e035bf025e49783c78259cbc9c6506
zetxx <zetxx@belogradcik.biz>
2018-05-24 11:45:23 +0300

    .

--- router/app/lcd/index.js
+++ router/app/lcd/index.js
@@ -1,16 +1,70 @@
 'use strict';
-// const oled = require('oled-js-pi');
 const draw = require('./draw');
+const i2c = require('i2c-bus');
+const Oled = require('oled-i2c-bus');
 
-module.exports = () => {
-    var d = draw(128, 64, '1_8x8');
-    d.addText('NET:4G ✓ | VPN:✗', 0);
-    d.addText('google: 1024ms', 8);
-    d.addText(`${String.fromCharCode(24)}1024Mb ${String.fromCharCode(25)}1024Mb`, 17);
-    d.addText('USED: 56%', 25);
-    d.addGraph([new Array(126).fill(0).map((v, idx) => idx), new Array(126).fill(0).map((v, idx) => 100 - idx)], 0, 34, 64);
-    console.log((new Array(128)).fill('-').join(''));
-    console.log(d.textart());
-    console.log((new Array(128)).fill('-').join(''));
-    console.log(JSON.stringify(d.getPoints()));
+const width = 128;
+const height = 64;
+const address = 0x3C;
+var oled;
+
+const getPixelCoords = ({gsmNetwork, gsmNetworkStatus, vpnStatus, ping, trafficUp, trafficDown, trafficUsed, graph}) => {
+    var d = draw(width, height, '1_8x8');
+    d.addText(`NET:${gsmNetwork}G ${gsmNetworkStatus === 'connected' ? '✓' : '✗'} | VPN:${vpnStatus === 'connected' ? '✓' : '✗'}`, 0);
+    d.addText(`google: ${ping}`, 8);
+    d.addText(`${String.fromCharCode(24)}${trafficUp} ${String.fromCharCode(25)}${trafficDown}`, 17);
+    d.addText(`traffic: ${trafficUsed}%`, 25);
+    d.addGraph(graph, 0, 34, 64);
+    return d.getPoints();
+};
+
+const i2cInit = () => {
+    return new Promise((resolve, reject) => {
+        const i2cBus = i2c.open(1, (err) => (err && reject(err)) || resolve(new Oled(i2cBus, {width, height, address})));
+    });
+};
+
+const isReady = () => {
+    return new Promise((resolve, reject) => oled._waitUntilReady(() => resolve(oled)));
+};
+
+const pullData = (sequelize) => {
+    return Promise.resolve({
+        gsmNetwork: '4',
+        gsmNetworkStatus: 'connected',
+        ping: (Math.random() * 100000).toString().slice(0, 4) + 'ms',
+        trafficUp: (Math.random() * 100000).toString().slice(0, 4) + 'Mb',
+        trafficDown: (Math.random() * 100000).toString().slice(0, 6) + 'Mb',
+        trafficUsed: (Math.random() * 100000).toString().slice(0, 2),
+        graph: [new Array(126).fill(0).map((v, idx) => idx), new Array(126).fill(0).map((v, idx) => 100 - idx)]
+    });
+};
+
+const redraw = (sequelize) => {
+    Promise.resolve()
+        .then(isReady)
+        .then(() => oled.turnOffDisplay())
+        .then(isReady)
+        .then(() => oled.clearDisplay(true))
+        .then(isReady)
+        .then(() => pullData(sequelize))
+        .then((data) => oled.drawPixel(getPixelCoords(data), true))
+        .then(isReady)
+        .then(() => oled.update())
+        .then(isReady)
+        .then(() => oled.turnOnDisplay())
+        .then(isReady)
+        .then(() => console.log('done'));
+};
+
+i2cInit()
+    .then((o) => (oled = o));
+
+module.exports = (sequelize) => {
+    pullData(sequelize).then((data) => {
+
+    });
+    // setInterval(() => {
+    //     return oled && redraw(sequelize);
+    // }, 10000);
 };
