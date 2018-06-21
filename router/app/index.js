77ccc0b5b0dfaed4ccb15e007dc97822eac50863
Elin Angelov <zetxx@belogradchik.biz>
2018-06-21 14:06:45 +0300

    Update index.js

--- router/app/index.js
+++ router/app/index.js
@@ -3,11 +3,14 @@
 const http = require('./httpapi');
 const lcd = require('./lcd');
 const db = require('./db');
+if (!process.env.LCD_ADDR) {
+    throw new Error('LCD_ADDR env var should be set');
+}
 
 db(process.env.NODE_ENV || 'dev')
     .then((sequelize) => (
         http(sequelize) | // will write everything in sqlite
-        lcd(sequelize) // will read everything from sqlite
+        lcd(sequelize, process.env.LCD_ADDR) // will read everything from sqlite
     ))
     .catch(console.error);
 
