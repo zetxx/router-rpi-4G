288b8e07777cc39310baeee7fbcb1d10fbc910a7
Elin Angelov <zetxx@belogradchik.biz>
2018-06-21 14:19:36 +0300

    Update index.js

--- router/app/index.js
+++ router/app/index.js
@@ -1,8 +1,5 @@
 /* eslint-disable no-process-env */
 /* eslint-disable no-console */
-const http = require('./httpapi');
-const lcd = require('./lcd');
-const db = require('./db');
 if (!process.env.LCD_ADDR) {
     throw new Error('LCD_ADDR env var should be set');
 }
@@ -11,15 +8,26 @@ if (lcdAddress === 0 || isNaN(lcdAddress)) {
     throw new Error('LCD_ADDR env should contain hex value');
 }
 
+const http = require('./httpapi');
+const lcd = require('./lcd');
+const db = require('./db');
+const ping = require('./ping');
+
+
 db(process.env.NODE_ENV || 'dev')
     .then((sequelize) => (
         Promise.resolve(sequelize)
-            .then(http)// will write everything in sqlite
+            .then(http) // will write everything in sqlite
             .then(() => sequelize)
     ))
     .then((sequelize) => (
         Promise.resolve()
             .then(() => lcd(sequelize, process.env.LCD_ADDR)) // will read everything from sqlite
+            .then(() => sequelize)
+    ))
+    .then((sequelize) => (
+        Promise.resolve()
+            .then(() => ping(sequelize, 'www.google.com)) // will write everything in sqlite
             .then(() => sequelize)
     ))
     .catch(console.error);
