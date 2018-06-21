02a821d155c9b6141b4fc1df2261b141d1833a6f
Elin Angelov <zetxx@belogradchik.biz>
2018-06-21 14:06:49 +0300

    Update run-containers.sh

--- router/app/run-containers.sh
+++ router/app/run-containers.sh
@@ -2,6 +2,7 @@ docker run -it -d \
 --name app \
 --restart=unless-stopped \
 -e NODE_ENV=prod \
+-e LCD_ADDR=60 \
 -v ~/tmp/db.sqlite:/db/db.sqlite \
 --device /dev/i2c-0 \ #https://stackoverflow.com/questions/40265984/i2c-inside-a-docker-container
 -p 3000:3000 \
