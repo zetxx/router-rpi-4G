76606939d651a056008be27132bf58050ad508b6
Elin Angelov <zetxx@belogradchik.biz>
2018-07-05 20:43:51 +0300

    Update exec-root2.sh

--- exec-root2.sh
+++ exec-root2.sh
@@ -2,8 +2,11 @@
 
 cp /root/router-rpi-4G/etc/fstab /etc/
 user="zetxx"
+echo "$user  ALL=(ALL:ALL) ALL" >> /etc/sudoers
 #add user
 useradd -m -s /bin/zsh -d /home/$user $user
 #add user to specific groups
 usermod -a -G docker $user
 passwd $user
+
+userdel -f -r alarm
