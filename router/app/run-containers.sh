937cb175789d20803b9097271206a9b1f4854cfd
Elin Angelov <zetxx@belogradchik.biz>
2018-07-05 13:21:19 +0300

    Update run-containers.sh

--- router/app/run-containers.sh
+++ router/app/run-containers.sh
@@ -1,7 +1,6 @@
 docker run -it -d \
 --name app \
 --restart=unless-stopped \
--e NODE_ENV=prod \
 -e LCD_ADDR=60 \
 -v /usr/src/app/runtime:/home/zetxx/router-rpi-4G/router/app \
 -v ~/tmp/db.sqlite:/db/db.sqlite \
