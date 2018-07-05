5ad3d11235480066f09fe7ab255e193a76c56602
Elin Angelov <zetxx@belogradchik.biz>
2018-07-05 20:36:42 +0300

    Update exec-root1.sh

--- exec-root1.sh
+++ exec-root1.sh
@@ -4,4 +4,4 @@ pacman-key --init && \
 pacman-key --populate archlinuxarm && \
 pacman -Suy --noconfirm git ntp docker curl openssh openvpn iptraf-ng sudo zsh i2c-tools lm_sensors && \
 cd ~ && \
-git clone https://github.com/zetxx/router-rpi-4G.git
+git clone https://github.com/zetxx/router-rpi-4G.git /root/router-rpi-4G
