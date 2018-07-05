dbd277cc0f0988f23968b7588a9de396e424e7a5
Elin Angelov <zetxx@belogradchik.biz>
2018-07-05 18:13:39 +0300

    Update run-containers.sh

--- router/app/run-containers.sh
+++ router/app/run-containers.sh
@@ -2,8 +2,9 @@ docker run -it -d \
 --name app \
 --restart=unless-stopped \
 -e LCD_ADDR=60 \
--v /usr/src/app/runtime:/home/zetxx/router-rpi-4G/router/app \
--v ~/tmp/db.sqlite:/db/db.sqlite \
---device /dev/i2c-0 \ #https://stackoverflow.com/questions/40265984/i2c-inside-a-docker-container
+-v <host_dir>:/usr/src/app/runtime \
+-v <host_db_dir>/db.sqlite:/db/db.sqlite \
+#https://stackoverflow.com/questions/40265984/i2c-inside-a-docker-container
+--device /dev/i2c-1 \
 -p 3000:3000 \
 nodejs
