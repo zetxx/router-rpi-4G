0ce0c056bc1908bb4bc3fd12aa0547074d32b5c8
Elin Angelov <zetxx@belogradchik.biz>
2018-07-05 20:14:07 +0300

    Update exec-root1.sh

--- exec-root1.sh
+++ exec-root1.sh
@@ -2,4 +2,5 @@
 #install neceraly software
 pacman-key --init && \
 pacman-key --populate archlinuxarm && \
-pacman -Suy --noconfirm git ntp docker curl openssh openvpn iptraf-ng sudo zsh i2c-tools lm_sensors
+pacman -Suy --noconfirm git ntp docker curl openssh openvpn iptraf-ng sudo zsh i2c-tools lm_sensors && \
+git clone git@github.com:zetxx/router-rpi-4G.git
