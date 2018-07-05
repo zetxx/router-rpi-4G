d6c2cadddd35848bf0db0699ef3e408d1fc70afe
Elin Angelov <zetxx@belogradchik.biz>
2018-07-05 20:11:55 +0300

    Update and rename exec-root.sh to exec-root1.sh

--- exec-root1.sh
+++ exec-root1.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-
 #install neceraly software
+pacman-key --init && \
+pacman-key --populate archlinuxarm && \
 pacman -Suy --noconfirm git ntp docker curl openssh openvpn iptraf-ng sudo zsh i2c-tools lm_sensors
+
+
 systemctl enable docker.service
 systemctl start docker.service
-user="zetxx"
+#user="zetxx"
 #add user
 useradd -m -s /bin/zsh -d /home/$user $user
 #add user to specific groups
