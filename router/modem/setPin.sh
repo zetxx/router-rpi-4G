78573ebdd45bac2ee813a07f19b188d6ee2a4940
Elin Angelov <zetxx@belogradchik.biz>
2018-07-05 21:49:52 +0300

    Update setPin.sh

--- router/modem/setPin.sh
+++ router/modem/setPin.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 sleep 10
 
-curl -d "isTest=false&goformId=ENTER_PIN&PinNumber=1234" -X POST -k -o /tmp/pin_set_response http://192.168.0.1/goform/goform_set_cmd_process
+curl -d "isTest=false&goformId=ENTER_PIN&PinNumber=0000" -X POST -k -o /tmp/pin_set_response http://192.168.0.1/goform/goform_set_cmd_process
