70a297acc7fe81aa1ce4a91f9b551628f8e6999a
elin.angelov <elin.angelov@softwaregroup-bg.com>
2018-07-06 21:51:36 +0300

    vpn status prepare

--- router/modem/router-rpi-4g-status.sh
+++ router/modem/router-rpi-4g-status.sh
@@ -6,3 +6,13 @@
 #ping status
 PR="$(ping -c 2 www.google.com | tail -1| awk -F '/' '{print $5}' | cut -f1 -d'.')"
 curl -X PUT -d 'host=google&time='"$PR"'ms' http://localhost:3000/ping/status
+
+#VPN Status
+zz="$((
+echo open 127.0.0.1 6001
+sleep 1
+echo state
+sleep 1
+echo exit
+) | telnet)"
+echo $zz
