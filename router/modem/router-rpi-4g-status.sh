319603da1f054139d06590394da6d90d7286a104
Elin Angelov <zetxx@belogradchik.biz>
2018-07-10 21:17:05 +0300

    Update router-rpi-4g-status.sh

--- router/modem/router-rpi-4g-status.sh
+++ router/modem/router-rpi-4g-status.sh
@@ -8,11 +8,12 @@ PR="$(ping -c 2 www.google.com | tail -1| awk -F '/' '{print $5}' | cut -f1 -d'.
 curl -X PUT -d 'host=google&time='"$PR"'ms' http://localhost:3000/ping/status
 
 #VPN Status
-zz="$((
-echo open 127.0.0.1 6001
-sleep 1
-echo state
-sleep 1
-echo exit
-) | telnet)"
-echo $zz
+#zz="$((
+#echo open 127.0.0.1 6001
+#sleep 1
+#echo state
+#sleep 1
+#echo exit
+#) | telnet)"
+#echo $zz
+curl -X PUT -d 'active=true' http://localhost:3000/vpn/status
