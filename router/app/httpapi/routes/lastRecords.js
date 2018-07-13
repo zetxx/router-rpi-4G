29a3b31b7eefe27e17b5f4a20874eaafe58e54a5
zetxx <zetxx@belogradcik.biz>
2018-07-13 20:18:46 +0300

    fix: status

--- router/app/httpapi/routes/lastRecords.js
+++ router/app/httpapi/routes/lastRecords.js
@@ -12,10 +12,11 @@ module.exports = (server, sequelize) => (server.route({
         tags: ['api'],
         handler: (request, h) => {
             const orderAndLimit = {order: [['id', 'DESC']], limit: parseInt(request.params.n)};
+
             return Promise.all([
-              getVpnStatus(sequelize).find(orderAndLimit).then((r) => ({vpn: r})),
-              getGsmStatsModel(sequelize).find(orderAndLimit).then((r) => ({gsm: r})),
-              getPingStatsModel(sequelize).find(orderAndLimit).then((r) => ({ping: r}))
+                getVpnStatusModel(sequelize).findAll(orderAndLimit).then((r) => ({vpn: r})),
+                getGsmStatusModel(sequelize).findAll(orderAndLimit).then((r) => ({gsm: r})),
+                getPingStatusModel(sequelize).findAll(orderAndLimit).then((r) => ({ping: r}))
             ])
             .then((r) => r.reduce((a, c) => (Object.assign(a, c)), {}));
         },
@@ -23,7 +24,9 @@ module.exports = (server, sequelize) => (server.route({
             params: {
                 n: Joi.number().required().example(10)
             },
-            failAction: (request, h, err) => ((err.isJoi && h.response(JSON.stringify(err && err.details)).code(400).takeover()) || h.response(err).takeover())
+            failAction: (request, h, err) => {
+                return (err.isJoi && h.response(JSON.stringify(err && err.details)).code(400).takeover()) || h.response(err).takeover();
+            }
         }
     }
 }));
