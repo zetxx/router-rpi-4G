d600a40fa5e281be1c260f508b993685670c77a6
Elin Angelov <zetxx@belogradchik.biz>
2018-07-16 18:26:19 +0300

    Update status.js

--- router/app/httpapi/routes/vpn/status.js
+++ router/app/httpapi/routes/vpn/status.js
@@ -15,7 +15,8 @@ module.exports = (server, sequelize) => (server.route({
         },
         validate: {
             payload: {
-                active: Joi.bool().required()
+                active: Joi.bool(),
+                string: Joi.bool()
             },
             failAction: (request, h, err) => ((err.isJoi && h.response(JSON.stringify(err && err.details)).code(400).takeover()) || h.response(err).takeover())
         }
