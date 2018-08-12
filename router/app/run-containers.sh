d954bcc9b5ff2ca979fa2536e98afa213017bd4c
zetxx <zetxx@belogradcik.biz>
2018-08-12 14:46:40 +0300

    fix: prepare rethinkdb

--- router/app/run-containers.sh
+++ router/app/run-containers.sh
@@ -3,6 +3,7 @@
 docker run -it -d \
 --name app \
 --restart=unless-stopped \
+--link=rethink-4g \
 -e LCD_ADDR=60 \
 -v /home/zetxx/router-rpi-4G/router/app:/usr/src/app/runtime \
 -v /mounts/store1/router-rpi-4G/db/:/db/ \
@@ -10,15 +11,33 @@ docker run -it -d \
 -p 3000:3000 \
 i2c
 
+docker run -it -d \
+--restart=unless-stopped \
+-v "/store/data/rethink/4g-router:/data" \
+-p 8080:8080 \
+--name rethink-4g \
+rethinkdb
 
 ## TMP
-
+    ### APP
 docker run -it -d \
 --name app \
 --restart=unless-stopped \
+--link=rethink-4g \
 -e LCD_ADDR=60 \
--v /store/31.0GB/projects/router-rpi-4G/router/app:/usr/src/app/runtime \
+-v "/store/31.0GB/projects/router-rpi-4G/router/app:/usr/src/app/runtime" \
 -v /store/db.sqlite/:/db/ \
 --device /dev/i2c-1 \
 -p 3000:3000 \
 i2c
+
+    ### db
+docker run -it -d \
+--restart=unless-stopped \
+-v "/store/data/rethink/4g-router:/data" \
+-p 8080:8080 \
+--name rethink-4g \
+rethinkdb
+
+## build RPI
+docker run -it --rm rethinkdb /bin/bash
