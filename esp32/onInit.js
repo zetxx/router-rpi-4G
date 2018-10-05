c4758aace342eab45b447c786b73c6505900a07a
Elin Angelov <zetxx@belogradchik.biz>
2018-10-05 11:30:12 +0300

    Update onInit.js

--- esp32/onInit.js
+++ esp32/onInit.js
@@ -1,19 +1,22 @@
-const watchPin = D14;
-const batteryPin = D15;
-const powerPin = D33;
-[batteryPin, powerPin].map((pin) => pin.mode('output'));
-watchPin.mode('input');
+function onInit () {
+    const watchPin = D14;
+    const batteryPin = D15;
+    const powerPin = D33;
+    [batteryPin, powerPin].map((pin) => pin.mode('output'));
+    watchPin.mode('input');
 
-const stateChange = (pin) => {
-    console.log('watched pin state:', pin);
-    if(pin.state) {
-        powerPin.write(true);
-        batteryPin.write(false);
-    } else {
-        powerPin.write(false);
-        batteryPin.write(true);
+    const stateChange = (pin) => {
+        console.log('watched pin state:', pin);
+        if(pin.state) {
+            powerPin.write(true);
+            batteryPin.write(false);
+        } else {
+            powerPin.write(false);
+            batteryPin.write(true);
+        }
+    };
+
+    stateChange({state: watchPin.read()});
+
+    setWatch(stateChange, watchPin, {repeat: true, edge: 'both', data: watchPin});
 };
-
-stateChange({state: watchPin.read()});
-
-setWatch(stateChange, watchPin, {repeat: true, edge: 'both', data: watchPin});
