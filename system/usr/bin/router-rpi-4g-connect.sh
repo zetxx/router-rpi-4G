90647b82c2a5f55fcff6b9fb64e945545f76695d
Elin Angelov <zetxx@belogradchik.biz>
2018-10-10 21:02:15 +0300

    Update router-rpi-4g-connect.sh

--- system/usr/bin/router-rpi-4g-connect.sh
+++ system/usr/bin/router-rpi-4g-connect.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 while true; do
-  if curl -m 2 http://192.168.0.1/index.html | grep --quiet "{" ; then
+  if curl -m 2 http://10.21.21.1/index.html | grep --quiet "{" ; then
     echo "online"
     sleep 10
     ## PIN
@@ -9,18 +9,18 @@ while true; do
     #-d "isTest=false&goformId=ENTER_PIN&PinNumber=0000" \
     #-H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
     #-H "X-Requested-With: XMLHttpRequest" \
-    #-H "Origin: http://10.0.10.1" \
-    #-H "Referer: http://10.0.10.1/index.html" \
-    #http://10.0.10.1/goform/goform_set_cmd_process
+    #-H "Origin: http://10.21.21.1" \
+    #-H "Referer: http://10.21.21.1/index.html" \
+    #http://10.21.21.1/goform/goform_set_cmd_process
     #sleep 1
     ## MODE
     curl -X POST \
     -d "goformId=SET_BEARER_PREFERENCE&BearerPreference=WCDMA_AND_LTE" \
     -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
     -H "X-Requested-With: XMLHttpRequest" \
-    -H "Origin: http://10.0.10.1" \
-    -H "Referer: http://10.0.10.1/index.html" \
-    http://10.0.10.1/goform/goform_set_cmd_process
+    -H "Origin: http://10.21.21.1" \
+    -H "Referer: http://10.21.21.1/index.html" \
+    http://10.21.21.1/goform/goform_set_cmd_process
     break
   else
     echo "waiting 5 sec"
@@ -33,6 +33,6 @@ done
 #-d "goformId=SET_CONNECTION_MODE&ConnectionMode=auto_dial" \
 #-H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
 #-H "X-Requested-With: XMLHttpRequest" \
-#-H "Origin: http://10.0.10.1" \
-#-H "Referer: http://10.0.10.1/index.html" \
-#http://10.0.10.1/goform/goform_set_cmd_process
+#-H "Origin: http://10.21.21.1" \
+#-H "Referer: http://10.21.21.1/index.html" \
+#http://10.21.21.1/goform/goform_set_cmd_process
