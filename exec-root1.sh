4eceb16448d9fd544c36db0be64402cc1dec37d7
Elin Angelov <zetxx@belogradchik.biz>
2018-10-13 13:59:56 +0300

    fix: added package

--- exec-root1.sh
+++ exec-root1.sh
@@ -8,7 +8,7 @@ chmod 0777 -R /mounts/store1
 #install neceraly software
 pacman-key --init
 pacman-key --populate archlinuxarm
-pacman -Suy --noconfirm --needed git ntp docker curl openssh openvpn iptraf-ng sudo zsh i2c-tools lm_sensors base-devel archey3 cron vim
+pacman -Suy --noconfirm --needed git ntp docker curl openssh openvpn iptraf-ng sudo zsh i2c-tools lm_sensors base-devel archey3 cron vim dosfstools
 pacman -R lvm2
 
 git clone https://github.com/zetxx/router-rpi-4G.git /root/router-rpi-4G
