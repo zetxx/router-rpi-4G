1e1e07fe2d5e303f81fe2ac195c9fd18bf85fc10
zetxx <zetxx@belogradcik.biz>
2019-04-21 21:49:16 +0300

    fix: cleanup

--- app/lcd/fonts/index.js
+++
@@ -1,2 +0,0 @@
-const fs = require('fs');
-module.exports = fs.readdirSync('./lcd/fonts').filter((fn) => fn.endsWith('.json')).reduce((fonts, c) => Object.assign(fonts, {[c.split('.json').join('')]: require(`./${c}`)}), {});
