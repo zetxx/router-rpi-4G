14fc2f1517b07628bf90b01e9ea59d215b1ad3ab
zetxx <zetxx@belogradcik.biz>
2019-05-22 22:08:38 +0300

    fix: testing

--- app/screenTest.js
+++ app/screenTest.js
@@ -1,14 +1,14 @@
 const oledFont5x7 = require('oled-font-5x7');
-var rstGpioId = 22;
-var dcGpioId = 18;
+var rstGpioId = 25;
+var dcGpioId = 24;
 const ssd1351 = new (require('ssd1351').Ssd1351)(rstGpioId, dcGpioId);
 
 const test = async() => {
     try {
         await ssd1351.turnOnDisplay();
         ssd1351.clearDisplay();
-        ssd1351.setCursor(10, 10);
-        await ssd1351.writeString(oledFont5x7, 4, '12:12', { r: 255, g: 255, b: 255 });
+        ssd1351.setCursor(0, 0);
+        await ssd1351.writeString(oledFont5x7, 2, 'IRI RI', { r: 255, g: 255, b: 255 });
         await ssd1351.updateScreen();
         console.log('finished');
     } catch (e) {
@@ -17,3 +17,12 @@ const test = async() => {
 };
 
 test();
+
+// ssd1351.clearDisplay();
+// ssd1351.setCursor(0, 0);
+// ssd1351.writeString(oledFont5x7, 2, 'IRI RI', { r: 255, g: 0, b: 0 });
+// ssd1351.setCursor(0, 40);
+// ssd1351.writeString(oledFont5x7, 2, 'IRI RI', { r: 0, g: 255, b: 0 });
+// ssd1351.setCursor(0, 80);
+// ssd1351.writeString(oledFont5x7, 2, 'IRI RI', { r: 0, g: 0, b: 255 });
+// ssd1351.updateScreen();
