ca5808925415eb8bd34c29f8b73064e965bb6f8f
zetxx <zetxx@belogradcik.biz>
2019-05-31 09:59:26 +0300

    fix: pb

--- app/screentest.js
+++ app/screentest.js
@@ -6,27 +6,30 @@ const height = 128;
 const width = 128;
 const hw = height * width;
 var pixelsBuffer = Array.from({length: hw * 2}).fill(0);
-
-const f = async() => {
+const getPixelBuffer = async() => {
     let myImage = await jimp.read(path.join('./', 'screenshot.png'));
     await myImage.rgba(false);
     var scanStop = (hw - 1) * 4;
-    await new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         myImage.scan(0, 0, height, width, function(x, y, idx) {
             const bytes = Ssd1351.convertRgbColourToRgb565(this.bitmap.data[idx + 0], this.bitmap.data[idx + 1], this.bitmap.data[idx + 2], this.bitmap.data[idx + 3]);
             pixelsBuffer[idx / 2] = bytes[0];
             pixelsBuffer[idx / 2 + 1] = bytes[1];
             if (scanStop === idx) {
-                resolve(1);
+                resolve(pixelsBuffer);
             }
         });
     });
+};
+
+const f = async() => {
+    var pb = await getPixelBuffer();
     console .log('turnOnDisplay');
     await oled.turnOnDisplay();
     console .log('setCursor');
     await oled.setCursor(0, 0);
     console .log('setRawData');
-    await oled.setRawData(pixelsBuffer);
+    await oled.setRawData(pb);
     console .log('updateScreen');
     await oled.updateScreen();
 };
