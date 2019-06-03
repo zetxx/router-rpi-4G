56de6665dfa47d8c8efed89110955ac90be46137
zetxx <zetxx@belogradchik.biz>
2019-06-03 13:50:38 +0300

    fix: cleanup

--- app/Nodes/ScreenControl/oled/fonts/index.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-module.exports = fs.readdirSync(__dirname).filter((fn) => fn.endsWith('.json')).reduce((fonts, c) => Object.assign(fonts, {[c.split('.json').join('')]: require(`./${c}`)}), {});
