199d9b8bca5fccaf4e13952277a43483cde0d834
zetxx <zetxx@belogradchik.biz>
2019-06-03 13:51:02 +0300

    styles

--- app/Nodes/ScreenControl/http/screen.js
+++ app/Nodes/ScreenControl/http/screen.js
@@ -15,11 +15,11 @@ function runAll() {
         })
     })
         .then((data) => {
-            var list = data.traffic.map(({date, ...rest}) => ({date: new Date(date), ...rest}));
+            var list = data.graphData.map(({date, ...rest}) => ({date: new Date(date), ...rest}));
             var svgWidth = (parseInt(d3.select('#plate').style('width').slice(0, -2)));
             var svgHeight = (parseInt(d3.select('#plate').style('height').slice(0, -2)));
             // set the dimensions and margins of the graph
-            var margin = {top: 3, right: 21, bottom: 18, left: 21};
+            var margin = {top: 9, right: 12, bottom: 3, left: 11};
             var width = svgWidth - margin.left - margin.right;
             var height = svgHeight - margin.top - margin.bottom;
 
@@ -27,28 +27,25 @@ function runAll() {
             var x = d3.scaleTime().range([0, width]);
             var y0 = d3.scaleLinear().range([height, 0]);
             var y1 = d3.scaleLinear().range([height, 0]);
+            var y2 = d3.scaleLinear().range([height, 0]);
 
             // define the upload area
             var uploadArea = d3.area()
-                .x(function(d) { return x(d.date); })
+                .x((d) => x(d.date))
                 .y0(height)
-                .y1(function(d) { return y0(d.upload); });
+                .y1((d) => y0(d.upload));
 
-            // define the upload area
+            // define the download area
             var downloadArea = d3.area()
-                .x(function(d) { return x(d.date); })
+                .x((d) => x(d.date))
                 .y0(height)
-                .y1(function(d) { return y1(d.download); });
+                .y1((d) => y1(d.download));
 
-            // // define the upload line
-            // var uploadValue = d3.line()
-            //     .x(function(d) { return x(d.date); })
-            //     .y(function(d) { return y0(d.upload); });
-
-            // // define the download line
-            // var downloadValue = d3.line()
-            //     .x(function(d) { return x(d.date); })
-            //     .y(function(d) { return y1(d.download); });
+            // define the ping area
+            var pingArea = d3.area()
+                .x((d) => x(d.date))
+                .y0(height)
+                .y1((d) => y2(d.ping));
 
             // append the svg obgect to the body of the page
             // appends a 'group' element to 'svg'
@@ -60,75 +57,55 @@ function runAll() {
                 .attr('transform',
                     'translate(' + margin.left + ',' + margin.top + ')');
 
-            // format the data
-            list.map(function(d) {
-                d.date = d.date;
-                d.upload = +d.upload;
-                d.download = +d.download;
-            });
-
             // Scale the range of the data
-            x.domain(d3.extent(data.traffic, function(d) {
-                return d.date;
-            }));
-            y0.domain([0, d3.max(data.traffic, function(d) {
-                return Math.max(d.upload);
-            })]);
-            y1.domain([0, d3.max(data.traffic, function(d) {
-                return Math.max(d.download);
-            })]);
+            x.domain(d3.extent(list, (d) => d.date));
+            y0.domain([0, d3.max(list, (d) => Math.max(d.upload))]);
+            y1.domain([0, d3.max(list, (d) => Math.max(d.download))]);
+            y2.domain([0, d3.max(list, (d) => Math.max(d.ping))]);
 
             // add upload area
             svg.append('path')
-                .data([data.traffic])
+                .data([list])
                 .attr('class', 'area upload')
                 .attr('d', uploadArea);
 
             // add download area
             svg.append('path')
-                .data([data.traffic])
+                .data([list])
                 .attr('class', 'area download')
                 .attr('d', downloadArea);
 
-            // // Add the download path.
-            // svg.append('path')
-            //     .data([data.traffic])
-            //     .attr('class', 'line upload')
-            //     .attr('d', uploadValue);
-
-            // // Add the upload path.
-            // svg.append('path')
-            //     .data([data.traffic])
-            //     .attr('class', 'line download')
-            //     .attr('d', downloadValue);
+            // add ping area
+            svg.append('path')
+                .data([list])
+                .attr('class', 'area ping')
+                .attr('d', pingArea);
 
             // Add the X Axis
             svg.append('g')
                 .attr('class', 'axis bottom')
                 .attr('transform', 'translate(0,' + height + ')')
-                .call(d3.axisBottom(x));
+                .call(d3.axisBottom(x).tickSize(2));
 
             // Add the Y0 Axis
             svg.append('g')
                 .attr('class', 'axis upload')
-                .call(d3.axisLeft(y0))
+                .call(d3.axisLeft(y0).tickSize(1).tickPadding(0))
                 .append('text')
-                .attr('y', 86)
-                .attr('x', -4)
-                .html('M&#8679;');
+                .attr('y', -4)
+                .attr('x', 22)
+                .html(data.trafficMetrics.tx + '&#8679;');
 
             // Add the Y1 Axis
             svg.append('g')
                 .attr('class', 'axis download')
                 .attr('transform', 'translate( ' + width + ', 0 )')
-                .call(d3.axisRight(y1))
+                .call(d3.axisRight(y1).tickSize(2).tickPadding(0))
                 .append('text')
-                .attr('y', 86)
-                .attr('x', 4)
-                .html('M&#8681;');
+                .attr('y', -4)
+                .attr('x', -75)
+                .html(data.trafficMetrics.rx + '&#8681;');
             return 1;
         })
-        .catch((e) => {
-            console.log(e);
-        });
+        .catch(console.error);
 };
