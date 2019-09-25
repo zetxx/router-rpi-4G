a687762bfb1910f54e5a2e021921ffe7fe3c471b
zetxx <zetxx@belogradchik.biz>
2019-09-25 18:19:23 +0300

    fix: naming

--- esp32/Adafruit-Feather-HUZZAH32/micropython/main.py
+++ esp32/Adafruit-Feather-HUZZAH32/micropython/main.py
@@ -18,19 +18,19 @@ rpiChecks = []
 def powerWatch(p):
     print('=====================powerWatch==============================')
     if p.value() == 0:# if line is down
-        chargeLine.off()
+        toBattery.off()
         powerLine.off()
-        batteryLine.on()
+        fromBattery.on()
     else:# if line is up
-        batteryLine.off()
-        chargeLine.on()
+        fromBattery.off()
+        toBattery.on()
         powerLine.on()
 
 def resetLoad():
     print('=====================resetLoad==============================')
-    Timer(-2).init(period=10000, mode=Timer.ONE_SHOT, callback=lambda t: powerWatch(highPowerLine))
+    Timer(-2).init(period=10000, mode=Timer.ONE_SHOT, callback=lambda t: powerWatch(powerGridDetection))
     powerLine.off()
-    batteryLine.off()
+    fromBattery.off()
 
 def netConn():
     print('=====================netCon==============================')
@@ -66,11 +66,11 @@ print('=====================Init==============================')
 wlan = WLAN(STA_IF)
 wlan.active(True)
 
-chargeLine = Pin(12, Pin.OUT, Pin.PULL_DOWN)
-batteryLine = Pin(13, Pin.OUT, Pin.PULL_DOWN)
+toBattery = Pin(12, Pin.OUT, Pin.PULL_DOWN)
+fromBattery = Pin(13, Pin.OUT, Pin.PULL_DOWN)
 powerLine = Pin(14, Pin.OUT, Pin.PULL_DOWN)
-highPowerLine = Pin(15, Pin.IN, Pin.PULL_DOWN)
+powerGridDetection = Pin(15, Pin.IN, Pin.PULL_DOWN)
 netConn()
-powerWatch(highPowerLine)
-highPowerLine.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=powerWatch)
+powerWatch(powerGridDetection)
+powerGridDetection.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=powerWatch)
 Timer(-1).init(period=30000, mode=Timer.PERIODIC, callback=lambda t: asyncio.get_event_loop().run_until_complete(checkRpi()))
