5778747fb2960b78816a678695f3ff4af34cf7fd
Elin Angelov <zetxx@belogradchik.biz>
2020-07-17 23:02:24 +0300

    Update router-rpi-4g-status.sh

--- system/home/zetxx/crontab/router-rpi-4g-status.sh
+++ system/home/zetxx/crontab/router-rpi-4g-status.sh
@@ -1,9 +1,8 @@
 #! /bin/sh
 
-printf "\nping status\n"
-PR="$(ping -c 2 www.google.com | tail -1| awk -F '/' '{print $5}' | cut -f1 -d'.')"
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"jsonrpc": "2.0","meta": {},"method": "stats.insert","params": {"type": "ping", "data": {"host": "google.com", "value": "'"$PR"'", "units": "ms"}}}' http://127.0.0.1:9004/JSONRPC/stats.insert
-# curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"jsonrpc": "2.0","meta": {},"method": "stats.insert","params": {"type": "ping", "data": {"host": "google.com", "value": "'"$PR"'", "units": "ms"}}}' http://127.0.0.1:9004/JSONRPC/stats.insert
+#printf "\nping status\n"
+#PR="$(ping -c 2 www.google.com | tail -1| awk -F '/' '{print $5}' | cut -f1 -d'.')"
+#curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"jsonrpc": "2.0","meta": {},"method": "stats.insert","params": {"type": "ping", "data": {"host": "google.com", "value": "'"$PR"'", "units": "ms"}}}' http://127.0.0.1:9004/JSONRPC/stats.insert
 
 printf "\nvpn status\n"
 #VPN Status
@@ -17,5 +16,4 @@ echo exit
 echo $vpnStat
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"jsonrpc": "2.0","meta": {},"method": "stats.insert","params": {"type": "vpn", "data": {"raw": "'"$vpnStat"'"}}}' http://127.0.0.1:9004/JSONRPC/stats.insert
-# curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"jsonrpc": "2.0","meta": {},"method": "stats.insert","params": {"type": "vpn", "data": {"raw": "dGVsbmV0PiBUcnlpbmcgMTI3LjAuMC4xLi4uCkNvbm5lY3RlZCB0byAxMjcuMC4wLjEuCkVzY2FwZSBjaGFyYWN0ZXIgaXMgJ15dJy4KPklORk86T3BlblZQTiBNYW5hZ2VtZW50IEludGVyZmFjZSBWZXJzaW9uIDEgLS0gdHlwZSAnaGVscCcgZm9yIG1vcmUgaW5mbwoxNTU0OTg3ODk2LENPTk5FQ1RFRCxTVUNDRVNTLDEwLjguMC4xLCwsLApFTkQK"}}}' http://127.0.0.1:9004/JSONRPC/stats.insert
 printf "\n"
