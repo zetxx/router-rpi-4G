95854bbd173c3540712bafcbbdbf57e9e77b6da4
Elin Angelov <zetxx@belogradchik.biz>
2020-07-17 23:25:45 +0300

    Update router-rpi-4g-status.sh

--- system/home/zetxx/crontab/router-rpi-4g-status.sh
+++ system/home/zetxx/crontab/router-rpi-4g-status.sh
@@ -15,5 +15,8 @@ echo exit
 ) | telnet | base64 -w0)"
 echo $vpnStat
 
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"jsonrpc": "2.0","meta": {},"method": "stats.insert","params": {"type": "vpn", "data": {"raw": "'"$vpnStat"'"}}}' http://127.0.0.1:9004/JSONRPC/stats.insert
+#curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"jsonrpc": "2.0","meta": {},"method": "stats.insert","params": {"type": "vpn", "data": {"raw": "'"$vpnStat"'"}}}' http://127.0.0.1:9004/JSONRPC/stats.insert
 printf "\n"
+
+# connect if dissconected
+curl -X POST -H "Referer: http://10.21.21.1/index.html" -H "Content-Type: application/x-www-form-urlencoded" -d "isTest=false&notCallback=true&goformId=CONNECT_NETWORK" http://10.21.21.1/goform/goform_set_cmd_process
